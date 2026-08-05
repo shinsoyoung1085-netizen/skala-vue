819e44f2c18ed0fb6422ea12c0b812ef38f0471e
shinsoyoung1085-netizen <shinsoyoung1085@gmail.com>
2026-08-05 23:13:39 +0900

    Revert "Replace with weather-components project (umbrella feature)"
    This reverts commit cd58807d1eaf173569fd7cd2d637368d8bd6407d.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import './style.css'
+import './assets/base.css'
 import App from './App.vue'
-import router from './router'
 
-createApp(App).use(createPinia()).use(router).use(ElementPlus).mount('#app')
+createApp(App).mount('#app')
